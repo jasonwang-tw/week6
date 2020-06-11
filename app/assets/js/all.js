01f236ab39be5e480086b697b7df6e6452a33717
JasonWang <ff1986755087@gmail.com>
2020-06-11 23:52:53 +0800

    0611

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,22 @@
-console.log('Hello!');
+$('.card-group').owlCarousel({
+  // center: true,
+  items: 3,
+  loop: false,
+  margin: 20,
+  stagePadding: 55,
+  // responsive: {
+  //   600: {
+  //     items: 4
+  //   }
+  // }
+});
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+$(window).scroll(function () { //隨時更新瀏覽器卷軸高度
+  if ($(window).scrollTop() > 200) {
+    $('.topMenu').addClass('bgBlack');
+    $('.logo').addClass('logo_w');
+  } else if ($(window).scrollTop() < 200) {
+    $('.topMenu').removeClass('bgBlack');
+    $('.logo').removeClass('logo_w');
+  }
 });
