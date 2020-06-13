e906bf2c64fe847c3d610be605fe443dd34863c7
JasonWang <ff1986755087@gmail.com>
2020-06-14 00:26:18 +0800

    Update 2020-06-13T16:26:12.237Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,28 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
+$('.card-group').owlCarousel({
+  // center: true,
+  items: 3,
+  loop: false,
+  margin: 20,
+  stagePadding: 55 // responsive: {
+  //   600: {
+  //     items: 4
+  //   }
+  // }
+
 });
+$(window).scroll(function () {
+  //隨時更新瀏覽器卷軸高度
+  if ($(window).scrollTop() > 100) {
+    $('.topMenu').addClass('bgBlack');
+    $('.logo').addClass('logo_w');
+  } else if ($(window).scrollTop() < 100) {
+    $('.topMenu').removeClass('bgBlack');
+    $('.logo').removeClass('logo_w');
+  }
+});
+$('.topMenu').scrollToFixed();
+var topMenu = $('.productNav').prevAll('.topMenu');
+$(topMenu).find('.nav').removeClass('text-white');
 //# sourceMappingURL=all.js.map
